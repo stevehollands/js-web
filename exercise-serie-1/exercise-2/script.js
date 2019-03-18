e304fdb6c90e88d2835bb7b65c52708e62aa70a3
stevehollands <steve.hollands1982@gmail.com>
2019-03-18 15:16:21 +0100

    s

--- exercise-serie-1/exercise-2/script.js
+++ exercise-serie-1/exercise-2/script.js
@@ -3,3 +3,31 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+
+document.getElementById("+").addEventListener("click", () => {
+    let value1 = document.getElementById("x").value;
+    let value2 = document.getElementById("y").value;
+    let value = (parseInt(value1) + parseInt(value2));
+    document.getElementById("resultaat").value = value;
+  });
+
+document.getElementById("-").addEventListener("click", () => {
+    let value1 = document.getElementById("x").value;
+    let value2 = document.getElementById("y").value;
+    let value = (parseInt(value1) - parseInt(value2));
+    document.getElementById("resultaat").value = value;
+  });
+
+document.getElementById("*").addEventListener("click", () => {
+    let value1 = document.getElementById("x").value;
+    let value2 = document.getElementById("y").value;
+    let value = (parseInt(value1) * parseInt(value2));
+    document.getElementById("resultaat").value = value;
+  });
+
+document.getElementById("/").addEventListener("click", () => {
+    let value1 = document.getElementById("x").value;
+    let value2 = document.getElementById("y").value;
+    let value = (parseInt(value1) / parseInt(value2));
+    document.getElementById("resultaat").value = value;
+  });
