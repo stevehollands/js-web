323fe381199de9ae57f099b69c4daac75d9fc49a
stevehollands <steve.hollands1982@gmail.com>
2019-03-18 11:35:30 +0100

    s

--- exercise-serie-1/exercise-1/script.js
+++ exercise-serie-1/exercise-1/script.js
@@ -3,3 +3,12 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+document.querySelector(".buttong").addEventListener("click", () => {
+    document.querySelector(".black").style.backgroundColor = "green";
+  });
+document.querySelector(".buttonr").addEventListener("click", () => {
+    document.querySelector(".black").style.backgroundColor = "red";
+  });
+document.querySelector(".buttonb").addEventListener("click", () => {
+    document.querySelector(".black").style.backgroundColor = "blue";
+  });
