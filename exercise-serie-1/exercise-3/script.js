d007626467bc80b0807788e359c24aaf869cd74e
stevehollands <steve.hollands1982@gmail.com>
2019-03-18 17:02:09 +0100

    s

--- exercise-serie-1/exercise-3/script.js
+++ exercise-serie-1/exercise-3/script.js
@@ -3,3 +3,22 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+function reset() {
+    document.getElementById("frm1").reset();
+  }
+
+function display() {
+
+    let name = document.getElementById("sname").value;
+    let power = document.getElementById("spower").value;
+    let motivation = document.getElementById("motiv").value;
+    let plan = document.getElementById("splan").value;
+
+    //erase Form and buttons
+
+
+    document.getElementById("frm1").innerHTML = "";
+
+
+
+}
