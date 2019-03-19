21b21ec69931973aa4535ca1324beaccb3260970
stevehollands <steve.hollands1982@gmail.com>
2019-03-19 10:53:36 +0100

    s

--- exercise-serie-1/exercise-4/script.js
+++ exercise-serie-1/exercise-4/script.js
@@ -3,3 +3,9 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+function calcAge(){
+    var d = new Date();
+    var year = new Date(document.getElementById("jos").value);
+    var age = d.getFullYear() - year.getFullYear();
+    document.getElementById("age").value = age;
+}
