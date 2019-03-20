d15ad6b938726f10a334d1b1fd2c1669f43e9a3a
stevehollands <steve.hollands1982@gmail.com>
2019-03-20 09:45:44 +0100

    s

--- exercise-serie-1/exercise-5/script.js
+++ exercise-serie-1/exercise-5/script.js
@@ -7,6 +7,7 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
+  
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
@@ -22,3 +23,43 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 1000); // Change image every second , 2000 for 2 seconds , etc
 }
+
+function setAspectRatio(img, expected) {
+    
+	// No support for IE8 and lower
+	if ( img.naturalWidth === 'undefined' ) return;
+	
+	// Get natural dimensions of image
+	var width = img.naturalWidth;
+	var height = img.naturalHeight;
+	
+	// Examine if width is larger than height then reduce the width but fix the height
+	if ( width > height ) {
+		img.style.width = (width / height * expected) + 'px';
+		img.style.height = expected + 'px';
+		
+		// horizontally center the image
+		img.style.transform = 'translatex(-' + parseInt((width / height * expected) - expected) + 'px)';
+		img.style.webkitTransform = 'translateX(-' + parseInt((width / height * expected) - expected) + 'px)';
+		return img;
+	}
+	
+	// Examine if height is larger than width then reduce the height but fix the width
+	else if ( height > width ) {
+		img.style.width = expected + 'px';
+		img.style.height = (height / width * expected) + 'px';
+		
+		// vertically center the image
+		img.style.transform = 'translateY(-' + parseInt((height / width * expected) - expected) + 'px)';
+		img.style.webkitTransform = 'translateY(-' + parseInt((height / width * expected) - expected) + 'px)';
+		return img;
+	}
+	
+	// Or return fix width and height
+	else {
+		img.style.width = expected + 'px';
+		img.style.height = expected + 'px';
+	}
+}
+
+ 
