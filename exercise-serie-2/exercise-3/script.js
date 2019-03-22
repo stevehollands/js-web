a8d09477d9ac932237c6bec85b641f14ffe5a7e4
stevehollands <steve.hollands1982@gmail.com>
2019-03-22 16:52:03 +0100

    s

--- exercise-serie-2/exercise-3/script.js
+++ exercise-serie-2/exercise-3/script.js
@@ -3,3 +3,16 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+var cookiecount = 0;
+
+function add () {
+
+cookiecount = cookiecount + 1 ;
+document.getElementById('text').value = cookiecount;
+document.title = cookiecount + "cookies";
+
+}
+
+function myFunction() {
+    location.reload();
+ }
--- exercise-serie-2/exercise-4/script.js
+++ exercise-serie-2/exercise-4/script.js
@@ -3,3 +3,16 @@ console.log("This script is linked properly!")
 
 
 //Write your JS code here...
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET','https://character-database.becode.xyz/', true)
+
+request.onload = function (){
+// Begin accessing JSON data here
+
+var data = JSON.parse(this.response)
+
+
+if (request.status >= 200 && request.status < 400){
+} 
